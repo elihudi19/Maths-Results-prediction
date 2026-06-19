1cafdbe65015dfc4ee99e349660bd9f9cd6d487e
elihudi <elihudi.eliamini@gmail.com>
2026-06-19 13:46:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ with st.sidebar:
         "Coefficient": [round(coef[0], 4), round(coef[1], 4), round(coef[2], 4)], 
             })
     st.dataframe(coef_df, hide_index=True, use_container_width=True)
-    st.write(f"**Intercept:** {intercept:.4f}          This means the probability to pass is starting at 0.10724 when all variable is zero. But in real world enterpretation teacher to student ratio can never be zero"),
+    st.write(f"**Intercept:** {intercept:.4f}          This means the probability to pass is starting at 0.10724 when all variable is zero Where In school Type Private= 0 and In Mock result F= 0 This occur during encoding process. But in real world enterpretation teacher to student ratio can never be zero"),
     st.markdown("---")
     st.subheader("INTERPRETATIONS")
     st.markdown(
